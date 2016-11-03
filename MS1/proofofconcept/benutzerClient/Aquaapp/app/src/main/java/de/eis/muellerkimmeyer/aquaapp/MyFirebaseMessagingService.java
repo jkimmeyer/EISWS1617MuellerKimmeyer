6a016dfb660cb2969ca3332d17de0863137ba3c7
mo-mueller <nono.game@googlemail.com>
2016-11-03 16:54:49 +0100

    PoC Benutzer Client hinzugefügt

---
+++ MS1/proofofconcept/benutzerClient/Aquaapp/app/src/main/java/de/eis/muellerkimmeyer/aquaapp/MyFirebaseMessagingService.java
@@ -0,0 +1,59 @@
+package de.eis.muellerkimmeyer.aquaapp;
+
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.support.v7.app.NotificationCompat;
+import android.util.Log;
+
+import android.R;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+import static com.google.android.gms.internal.zzs.TAG;
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+
+    public MyFirebaseMessagingService() {
+    }
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        // TODO(developer): Handle FCM messages here.
+        // If the application is in the foreground handle both data and notification messages here.
+        // Also if you intend on generating your own notifications as a result of a received FCM
+        // message, here is where that should be initiated. See sendNotification method below.
+        Log.d(TAG, "From: " + remoteMessage.getFrom());
+        Log.d(TAG, "Notification Message Body: " + remoteMessage.getNotification().getBody());
+
+        showNotification(remoteMessage.getData().get("message"));
+    }
+
+    /*
+    * QUELLENANGABE
+    * showNotification übernommen aus https://www.youtube.com/watch?v=LiKCEa5_Cs8
+    * Autor: Filip Vujovic, Abruf am: 29.10.2016
+    * Änderungen: -
+    */
+
+    private void showNotification(String message){
+
+        Intent i = new Intent(this,MainActivity.class);
+        i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+
+        PendingIntent pendingIntent = PendingIntent.getActivity(this,0,i,PendingIntent.FLAG_UPDATE_CURRENT);
+
+        NotificationCompat.Builder builder = (android.support.v7.app.NotificationCompat.Builder) new NotificationCompat.Builder(this)
+                .setAutoCancel(true)
+                .setContentTitle("Aquaapp")
+                .setContentText(message)
+                .setSmallIcon(R.drawable.btn_star)
+                .setContentIntent(pendingIntent);
+
+        NotificationManager manager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+
+        manager.notify(0,builder.build());
+
+    }
+}
